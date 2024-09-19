e94f61df97850ba716ab0583e0e9daf31e53488e
Nico Joun <nicojoun793@gmail.com>
2024-09-19 13:08:44 +0200

    commit initial

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          I enjoy eating ice creams on the moon.
         </p>
         <a
           className="App-link"
