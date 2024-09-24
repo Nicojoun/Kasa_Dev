d45208bdab0881c0246105bec6c0479ecc42e2a6
Nico Joun <nicojoun793@gmail.com>
2024-09-24 10:44:14 +0200

    ajout du logo et des liens dans le header (css pas terminé)

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,11 +1,22 @@
 import '../../styles/App.scss';  
+import logo from '../../assets/logo.png';  // Importation de l'image
+import { Link } from 'react-router-dom';  // Importation de Link pour la navigation
 
 function Header() {
   return (
     <div>
-        <p>
-          Ceci est mon header.
-        </p>
+      <img src={logo} alt="Logo" />  {/* Affichage de l'image */}
+      
+      <nav>  
+        <ul>
+          <li>
+            <Link to="/">Accueil</Link>  {/* Lien vers la page d'accueil */}
+          </li>
+          <li>
+            <Link to="/about">À propos</Link>  {/* Lien vers la page "À propos" */}
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 }
---
+++ src/pages/About/index.jsx
@@ -0,0 +1,15 @@
+import '../../styles/App.scss';  
+
+function About() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          à propos
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default About;
