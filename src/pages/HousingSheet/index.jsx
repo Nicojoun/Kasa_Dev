977a472fd97957c50c247a279bda2ee7334ee59e
Nico Joun <nicojoun793@gmail.com>
2024-09-24 14:59:46 +0200

    ajout de texte dans la page HousisngSheet

---
+++ src/pages/HousingSheet/index.jsx
@@ -0,0 +1,15 @@
+import '../../assets/styles/App.scss';  
+
+function HousingSheet() {
+  return (
+    <div className="app">
+      <header className="app-header">
+        <p>
+          fiche logements
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default HousingSheet;
