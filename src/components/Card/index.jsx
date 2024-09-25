97c40e5981764c174011e1da9febf76bc934aa16
Nico Joun <nicojoun793@gmail.com>
2024-09-25 15:01:22 +0200

    mise en page du composant Cart

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -4,8 +4,8 @@ import '../../assets/styles/Card.scss';
 function Card({ logement }) { // Récupérer le logement passé en prop
   return (
     <div className='card'>
-      <h2>{logement.title}</h2> {/* Affichage du titre de la location */}
-      <p>{logement.cover}</p> {/* Affichage de l'image de la location */}
+      <img src={logement.cover} alt={logement.title} className='card-image' /> {/* Affichage de l'image de la location */}
+      <h2 className='card-title'>{logement.title}</h2> {/* Affichage du titre de la location */}
     </div>
   );
 }
