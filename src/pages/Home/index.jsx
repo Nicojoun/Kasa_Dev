001e59ba94cf04846ed7c268114339c1b0a660f9
Nico Joun <nicojoun793@gmail.com>
2024-09-27 12:58:01 +0200

    mise en page partielle de la page logements

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,6 +3,7 @@ import '../../assets/styles/Home.scss';
 import Banner from '../../components/Banner';  // Importation du composant Banner
 import logements from '../../datas/logements.json'; // Assurez-vous d'importer le fichier JSON
 import Card from '../../components/Card';  // Importation du composant Card
+import { Link } from 'react-router-dom'; // Importer Link pour la navigation
 
 function Home() {
   return (
@@ -10,10 +11,12 @@ function Home() {
       <Banner />  {/* Affichage du composant Banner */}
       <div className='home'> 
         {logements.map(logement => (
-          <Card key={logement.id}> {/* Utilisation de Card avec children */}
-            <img src={logement.cover} alt={logement.title} className='card-image' /> {/* Affichage de l'image de la location */}
-            <h2 className='card-title'>{logement.title}</h2> {/* Affichage du titre de la location */}
-          </Card>
+          <Link className='home-linkHousing' key={logement.id} to={`/housing/${logement.id}`}>  {/* Navigation avec l'ID */}
+            <Card className='home-card'> {/* Utilisation de Card avec children */}
+              <img src={logement.cover} alt={logement.title} className='home-cardImage' /> {/* Affichage de l'image de la location */}
+              <h2 className='home-cardTitle'>{logement.title}</h2> {/* Affichage du titre de la location */}
+            </Card>
+          </Link>
         ))}
       </div>
     </div>
--- src/pages/HousingSheet/index.jsx
+++ src/pages/HousingSheet/index.jsx
@@ -1,11 +1,49 @@
+import React from 'react';
+import { useParams } from 'react-router-dom'; // Utiliser useParams pour récupérer l'ID de l'URL
+import logements from '../../datas/logements.json'; // Importer les données des logements
+import '../../assets/styles/HousingSheet.scss';
+
 function HousingSheet() {
+  const { id } = useParams(); // Récupérer l'ID depuis les paramètres de l'URL
+  const logement = logements.find(log => log.id === id); // Trouver le logement correspondant
+
+  // Fonction pour afficher les étoiles en fonction de la note
+  const renderStars = (rating) => {
+    const stars = [];
+    for (let i = 1; i <= 5; i++) {
+      stars.push(
+        <span key={i} className={`housingSheet-star ${i <= rating ? 'housingSheet-filled' : ''}`}>
+          ★
+        </span>
+      );
+    }
+    return stars;
+  };
+
   return (
-    <div className="app">
-      <header className="app-header">
-        <p>
-          fiche logements
-        </p>
-      </header>
+    <div className='housingSheet'>
+      <img className='housingSheet-image' src={logement.cover} alt={logement.title} /> {/* Affichage de l'image du logement */}
+      <h1 className='housingSheet-title'>{logement.title}</h1> {/* Affichage du titre du logement */}
+      <h2 className='housingSheet-location'>{logement.location}</h2> {/* Affichage du lieu du logement */}
+
+      {/* Affichage des tags */}
+      <div className='housingSheet-tagList'>
+        {logement.tags.map((tag, index) => (
+          <span key={index} className='housingSheet-tag'>{tag}</span>  // Affichage de chaque tag
+        ))}
+      </div>
+
+      {/* Affichage du nom et de la photo de l'hôte */}
+      <div className='housingSheet-host'>
+        <span className='housingSheet-hostName'>{logement.host.name}</span> {/* Nom de l'hôte */}
+        <img className='housingSheet-hostImage' src={logement.host.picture} alt={logement.host.name} /> {/* Photo de l'hôte */}
+      </div>
+
+      {/* Affichage de la note sous forme d'étoiles */}
+      <div className='housingSheet-rating'>
+        {renderStars(logement.rating)} {/* Appel de la fonction pour afficher les étoiles */}
+      </div>
+
     </div>
   );
 }
