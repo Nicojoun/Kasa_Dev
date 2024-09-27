1adbd0ab85ee7a0447fd61a2411a774c4aa952d1
Nico Joun <nicojoun793@gmail.com>
2024-09-27 10:26:59 +0200

    mise en page de la page NotFound

--- src/pages/NotFound/index.jsx
+++ src/pages/NotFound/index.jsx
@@ -1,11 +1,14 @@
+import { Link } from 'react-router-dom'; // Importer le composant Link de react-router-dom
+import '../../assets/styles/NotFound.scss';
+
 function NotFound() {
   return (
-    <div className="app">
-      <header className="app-header">
-        <p>
-          page introuvable
-        </p>
-      </header>
+    <div className="notFound">
+      <h1 className='notFound-404'>404</h1>
+      <p className='notFound-message'>Oups! La page que vous demandez n'existe pas.</p>
+      <Link to="/" className='notFound-linkBackHome'> {/* Lien vers la page d'accueil */}
+        Retourner sur la page d’accueil
+      </Link>
     </div>
   );
 }
