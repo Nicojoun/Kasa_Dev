d4e80594c1d678a3e3aec55edf2dbb299524ffe7
Nico Joun <nicojoun793@gmail.com>
2024-10-15 11:36:15 +0200

    ajout d'une div autour de Card dans HousingSheet

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -5,7 +5,9 @@ function Card({ cardSrc, cardTitle, cardAlt, cardClass }) {
   return (
     <div className={`card ${cardClass}`}>
         <img src={cardSrc} alt={cardAlt} className='card-image' /> {/* Affichage de l'image de la location */}
-        <h2 className='card-title'>{cardTitle}</h2>
+        <div className='card-info'>
+          <h2 className='card-title'>{cardTitle}</h2>
+        </div>   
     </div>
   );
 }
