0061b10585eceefbe75feb1f76bd4dbf3d79fcbf
Nico Joun <nicojoun793@gmail.com>
2024-10-17 10:37:51 +0200

    Host ressemble à la maquette

--- src/components/Host/index.jsx
+++ src/components/Host/index.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 import '../../assets/styles/Host.scss';
 
 const Host = ({ name, picture }) => {
+  const [firstName, lastName] = name.split(' ');
 
   return (
     <div className='host'>
-      <span className='host-name'>{name}</span> {/* Nom de l'hôte */}
+      <span className='host-name'>{firstName}<br />{lastName}</span> {/* Nom de l'hôte */}
       <img className='host-image' src={picture} alt={name} /> {/* Photo de l'hôte */}
     </div>
   );
